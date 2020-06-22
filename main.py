99d7eeedd2b7b6781249a28e290d71ef26946159
Loïc Augé <loic.auge@limayrac.fr>
2020-06-22 13:10:54 +0000

    FIN TP2

--- main.py
+++ main.py
@@ -9,7 +9,6 @@
 
 #ch = 'Esope reste ici et se repose'
 #print(ch[ch.find("i",0,len(ch))])
-
 """
 meteo = 'aujourd’hui, il fait %s , la vitesse du vent est %s ,l’humidité est %s' 
 tempDeg = "24°"
@@ -17,7 +16,6 @@ vitVent = "12km/h"
 hum = "45%"
 print(meteo %(tempDeg,vitVent,hum))
 """
-
 """
 meteo = 'aujourd’hui, il fait %s , la vitesse du vent est %s ,l’humidité est %s' 
 tempDeg = "beau"
@@ -25,18 +23,22 @@ vitVent = "faible"
 hum = "correcte"
 print(meteo %(tempDeg,vitVent,hum))
 """
-
 """
 chaineA = "cette chaine"
 chaineB = "contient %s caractères"
 chaineC = "par contre"
 chaineD = "celle-ci"
 print(chaineA,chaineB %len(chaineA+chaineB))
-print(chaineC,chaineB %len(chaineC+chaineB+chaineD),chaineD)
+print(chaineD,chaineB %len(chaineC+chaineB+chaineD),chaineC)
 """
 
 chaineA = "cette chaine"
 chaineB = "contient %s caractères"
 chaineC = "par contre"
 chaineD = "celle-ci"
-chaineBnew = chaineB.replace("%s","{}")
+chaineBnew = chaineB.replace("%s", "{}")
+chaineE = chaineA + " " + chaineBnew
+chaineF = chaineD + " " + chaineBnew + " " + chaineC
+
+print(chaineE.format(len(chaineE)))
+print(chaineF.format(len(chaineF)))
